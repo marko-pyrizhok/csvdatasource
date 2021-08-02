a60847578ecfc54020e17b1e3cb5a9d2171a456b
marko <public.marko.pyrizhok@gmail.com>
2021-08-02 13:59:02 +0300

    initial commit

---
+++ src/main/java/edu/home/csvasdatasource/CsvasdatasourceApplication.java
@@ -0,0 +1,13 @@
+package edu.home.csvasdatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvasdatasourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvasdatasourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/home/csvasdatasource/CsvasdatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.home.csvasdatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvasdatasourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
